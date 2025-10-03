2103801a750e78804e01d06932942746c465f6fe
Doris <sonicfasterone@gmail.com>
2025-10-03 05:08:20 +0300

    add function for simple fibonacci generator

--- fibonacci_generator.py
+++ fibonacci_generator.py
@@ -1,9 +1,13 @@
-number = 15
+def generate_fibonacci(number):
 
-fibonacci = [0, 1]
+    fibonacci = [0, 1]
 
-for i in range(2, number):
-    next_fib = fibonacci[-1] + fibonacci[-2]
-    fibonacci.append(next_fib)
+    for i in range(2, number):
+        next_fib = fibonacci[-1] + fibonacci[-2]
+        fibonacci.append(next_fib)
 
+    return fibonacci
+
+number = int(input("Введите количество чисел Фибоначчи для генерации: "))
+fibonacci = generate_fibonacci(number)
 print(f"Первые {number} чисел Фибоначчи: {fibonacci}")
