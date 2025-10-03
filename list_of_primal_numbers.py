98c46f838ca0e1194879650f5958a553fb95f8e6
Doris <sonicfasterone@gmail.com>
2025-10-03 03:47:29 +0300

    clean without comments

--- list_of_primal_numbers.py
+++ list_of_primal_numbers.py
@@ -3,7 +3,6 @@ def find_primes(limit):
     primes = []
     
     for number in range(2, limit + 1):
-       
         for divisor in range(2, number):
             if number % divisor == 0:
                 break
