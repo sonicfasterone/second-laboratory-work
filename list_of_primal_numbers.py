acb139454352e47140ce7a6f1caf8ee04b9c2c95
Doris <sonicfasterone@gmail.com>
2025-10-03 03:41:01 +0300

    task 2: list of prime numbers to 100

---
+++ list_of_primal_numbers.py
@@ -0,0 +1,19 @@
+def find_primes(limit):
+
+    primes = []
+    
+    for number in range(2, limit + 1):
+       
+        for divisor in range(2, number):
+            if number % divisor == 0:
+                break
+        else:
+
+            primes.append(number)
+    
+    return primes
+
+# Использование
+numbers = find_primes(100)
+print("Простые числа до 100:")
+print(numbers)
